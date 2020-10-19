c0ad98ceb7bb9ae34a0197642d64c97dc119ce1e
teekaygithub <tomoyuki.kato@yahoo.com>
2020-10-18 23:21:26 -0700

    Added window for logging exercises

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import UserWindow from "./component/users";
+import ExerciseWindow from "./component/exercises";
 
 function App() {
   return (
     <div className="container">
       <div id="user-window">
         <UserWindow />
+      </div>
+      <br />
+      <div id="exercise-window">
+        <ExerciseWindow />
       </div>
     </div>
   );
---
+++ src/component/exercises.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class ExerciseWindow extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div id="exercise-window">
+                <h3>Log your exercise here</h3>
+                <p>POST /api/exercise/add</p>
+                <form action="/api/exercise/add" method="POST">
+                    <input type="text" name="name" placeholder="What exercise did you do?" /><br />
+                    <input type="text" name="description" placeholder="Can you describe it?" /><br />
+                    <input type="text" name="duration" placeholder="How long? (in minutes)" /><br />
+                    <input type="text" name="date" placeholder="Enter date in yyyy-mm-dd format" /><br />
+                    <input type="submit" value="Submit" placeholder="" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ExerciseWindow;
