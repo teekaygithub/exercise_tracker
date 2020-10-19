5ee83ee1f046934747133d5307acec561385f0eb
teekaygithub <tomoyuki.kato@yahoo.com>
2020-10-18 23:44:27 -0700

    Basic style sheet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import UserWindow from "./component/users";
 import ExerciseWindow from "./component/exercises";
 
 function App() {
   return (
     <div className="container">
-      <div id="user-window">
+      <h1>Exercise Logger App</h1>
+      <div id="user-window-container">
         <UserWindow />
       </div>
       <br />
-      <div id="exercise-window">
+      <div id="exercise-window-container">
         <ExerciseWindow />
       </div>
     </div>
